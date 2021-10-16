77f1f647a5fe28857e1825aee5fe28ffa306f12e
Zach Yarbrough <zachyarbro@gmail.com>
2021-10-16 12:24:45 -0500

    add models, controllersm and routes

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 const router = require('express').Router();
-const htmlRoutes = require('./html/html-routes');
 
+const apiRoutes = require('./api');
+const htmlRoutes = require('./html-routes');
+
+router.use('/api', apiRoutes);
 router.use('/', htmlRoutes);
 
 router.use((req, res) => {
