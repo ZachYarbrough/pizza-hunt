da07f429f1ea8453dc7f57baa5d4ffa4d57a10b3
Zach Yarbrough <zachyarbro@gmail.com>
2021-10-17 08:30:58 -0500

    add comment model and routes

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -3,6 +3,12 @@ const { Pizza } = require('../models');
 const pizzaController = {
     getAllPizza(req, res) {
         Pizza.find({})
+            .populate({
+                path: 'comments',
+                select: '-__v'
+            })
+            .select('-__v')
+            .sort({ _id: -1 })
             .then(pizzaData => res.json(pizzaData))
             .catch(err => {
                 console.log(err);
@@ -12,6 +18,11 @@ const pizzaController = {
 
     getPizzaById({ params }, res) {
         Pizza.findOne({ _id: params.id })
+            .populate({
+                path: 'comments',
+                select: '-__v'
+            })
+            .select('-__v')
             .then(pizzaData => {
                 if (!pizzaData) {
                     res.status(404).json({ message: 'No pizza found with this id.' });
@@ -45,15 +56,15 @@ const pizzaController = {
 
     deletePizza({ params }, res) {
         Pizza.findOneAndDelete({ _id: params.id })
-          .then(pizzaData => {
-            if (!pizzaData) {
-              res.status(404).json({ message: 'No pizza found with this id!' });
-              return;
-            }
-            res.json(pizzaData);
-          })
-          .catch(err => res.status(400).json(err));
-      }
+            .then(pizzaData => {
+                if (!pizzaData) {
+                    res.status(404).json({ message: 'No pizza found with this id!' });
+                    return;
+                }
+                res.json(pizzaData);
+            })
+            .catch(err => res.status(400).json(err));
+    }
 };
 
 module.exports = pizzaController;
